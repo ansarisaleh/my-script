23a5d87e78fe94e3cfe8055a653939881aa59b40
Fanani M. Ihsan <contact@fanani.net>
2011-11-20 15:53:04 +0700

    fixing bug rsync/rsync-downloader.sh

--- rsync/rsync-downloader.sh
+++ rsync/rsync-downloader.sh
@@ -30,25 +30,31 @@
 # Configuration
 # Edit configuration bellow
 # directory setup must be end with "/"
-
 SRC_BACKUPS=/path/to/remote/backups/
 DEST_BACKUPS=/path/to/local/backups/
 IP=<value IP or HOSTNAME>
 USERNAME=<your_username>
-PATH_LOG=/path/to/log/
 
 # Global Variable
 # Don't edit this configuration
+DIR=`dirname $0`
+PATH_LOG=$DIR/logs/`basename $0`
+TMP_BACKUPS=$DIR/tmp/`basename $0`
 LOG_NAME=$(date +"%m-%d-%Y")
 BACKUPS_NAME=$(date +"%m-%d-%Y")
 NOW=$(date +"%m/%d/%Y %T")
 CMD=`which rsync`
-OPTION="-ahr -pog --delete --log-file="$PATH_LOG$LOG_NAME.log
-DIR=`dirname $0`
-TMP_BACKUPS=$DIR/tmp/`basename $0`
-echo "" >> $PATH_LOG$LOG_NAME.log
+OPTION="-ahr -pog --delete --log-file="$PATH_LOG/$LOG_NAME.log
+
+# mkdir temporary files, log path , and destination backups
+mkdir -p $TMP_BACKUPS
+mkdir -p $PATH_LOG
+mkdir -p $DEST_BACKUPS
+
+# Starting rsync
+echo "" >> $PATH_LOG/$LOG_NAME.log
 echo "----------- Starting of rsync -----------" >> $PATH_LOG/$LOG_NAME.log
-$CMD $OPTION -e "ssh -p 22" $USERNAME@$IP:$SRC_BACKUPS $TMP_BACKUPS
+$CMD $OPTION -e "ssh -p 1111" $USERNAME@$IP:$SRC_BACKUPS $TMP_BACKUPS
 echo $NOW "Starting create archive file . . ." >> $PATH_LOG/$LOG_NAME.log
 echo $NOW "Creating " $DEST_BACKUPS$BACKUPS_NAME.tar.bz2 >> $PATH_LOG/$LOG_NAME.log
 cd $TMP_BACKUPS
