3cc29120cc970e9d92617f47fe2302439b0fbdd1
Fanani M. Ihsan <contact@fanani.net>
2011-11-21 09:11:10 +0700

    start create fitur ignore database

--- mysql/full-mysql-backups.sh
+++ mysql/full-mysql-backups.sh
@@ -3,16 +3,16 @@
 # -----------
 # Description
 # -----------
-# this Is script for full backups mysql database
+# This Is script for full backups mysql database
 # Author  : Fanani M. Ihsan
 # Blog    : http://fanani.net
 # Version : 1.0
 
 # Edit configuration bellow
-MYSQL_USER=mysql_username
-MYSQL_PASS=mysql_password
-MYSQL_HOST=mysql_host
-DEST=/path/to/backups/
+MYSQL_USER=root
+MYSQL_PASS=kediri123
+MYSQL_HOST=127.0.0.1
+BACKUPS_DEST=/tmp/ok/
 
 # Binary file path
 MYSQL=`which mysql`
@@ -21,27 +21,29 @@ CHOWN=`which chown`
 CHMOD=`which chmod`
 
 # File name
-BACKUPS_NAME=$(date +"%m-%d-%Y")
+BACKUPS_NAME=$(date +"%%Y-m-%d")
 
 # Command option
-DBS=`$MYSQL -u$MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASS -Bse 'show databases'`
+DBS=`$MYSQL -h $MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS -Bse 'SHOW DATABASES'`
 
 # Ignore information_schema
 IGNORE="information_schema"
 
-# Temporary file
+# Script location
 DIR=`dirname $0`
+
+# Temporary file
 TMP_BACKUPS=$DIR/tmp/`basename $0`
 
 # Log
 PATH_LOG=$DIR/logs/`basename $0`
-LOG_NAME=$(date +"%m-%d-%Y")
-NOW=$(date +"%m/%d/%Y %T")
+LOG_NAME=$(date +"%Y-%m-%d")
+NOW=$(date +"%Y/%m/%d %T")
 
 # mkdir temporary files, log path , and destination backups
 mkdir -p $TMP_BACKUPS
 mkdir -p $PATH_LOG
-mkdir -p $DEST
+mkdir -p $BACKUPS_DEST
 
 echo "" >> $PATH_LOG/$LOG_NAME.log
 echo "----------- Starting backups -----------" >> $PATH_LOG/$LOG_NAME.log
@@ -53,16 +55,17 @@ do
 		continue
 	fi
 	echo $NOW "Backup database =>" $DB  >> $PATH_LOG/$LOG_NAME.log
-	$MYSQLDUMP -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $DB > $TMP_BACKUPS/$DB.sql
+	#$MYSQLDUMP -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $DB > $TMP_BACKUPS/$DB.sql
+	echo $DB
 done
 
 cd $TMP_BACKUPS
 echo $NOW "PWD to tmp directory -- " `pwd` >> $PATH_LOG/$LOG_NAME.log
 echo $NOW "Create archive ..." >> $PATH_LOG/$LOG_NAME.log
-tar -cjf $DEST$BACKUPS_NAME.tar.bz2 *
-echo $NOW "Create archive file done !" >> $PATH_LOG/$LOG_NAME.log
-echo $NOW "Remove temporary file ..." >> $PATH_LOG/$LOG_NAME.log
-rm -rf *.sql
-echo $NOW "Remove temporary file done" >> $PATH_LOG/$LOG_NAME.log
+#tar -cjf $BACKUPS_DEST$BACKUPS_NAME.tar.bz2 *
+#echo $NOW "Create archive file done !" >> $PATH_LOG/$LOG_NAME.log
+#echo $NOW "Remove temporary file ..." >> $PATH_LOG/$LOG_NAME.log
+#rm -rf *.sql
+#echo $NOW "Remove temporary file done" >> $PATH_LOG/$LOG_NAME.log
 echo "----------- Finish backups -----------" >> $PATH_LOG/$LOG_NAME.log
 echo "" >> $PATH_LOG/$LOG_NAME.log
