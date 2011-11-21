f6ed3c3b1599f40cf2e01e93a6d26695cde01f6e
Fanani M. Ihsan <contact@fanani.net>
2011-11-21 10:59:23 +0700

    update version rsync/rsync-downloader.sh from v1.1 to v1.2

--- rsync/rsync-downloader.sh
+++ rsync/rsync-downloader.sh
@@ -6,18 +6,20 @@
 # simple rsync script to backups from remote to local with ssh-keygen
 # Author  : Fanani M. Ihsan
 # Blog    : http://fanani.net
-# Version : 1.1
+# Version : 1.2
 
 # --------
 # HOWTO
 # --------
 # 1. Create ssh-keygen
 # 2. Register public key to remote server
-# 3. Setting Configuration bellow
+# 3. Setting Configuration
+# 4. Run the script
+#    $ sh rsync-downloader.sh
 
-# -----------------
-# Create SSH-Keygen
-# -----------------
+# ------------------------------------------
+# Create SSH-Keygen And Register Public Key
+# ------------------------------------------
 # Running this action in local terminal
 # $ ssh-keygen
 # $ ls ~/.ssh
@@ -36,17 +38,27 @@ IP=<value IP or HOSTNAME>
 USERNAME=<your_username>
 
 # Global Variable
-# Don't edit this configuration
+# Don't edit this configuration bellow
+
+# script location
 DIR=`dirname $0`
+
+# Logging
 PATH_LOG=$DIR/logs/`basename $0`
-TMP_BACKUPS=$DIR/tmp/`basename $0`
 LOG_NAME=$(date +"%Y-%m-%d")
+
+# temporary files
+TMP_BACKUPS=$DIR/tmp/`basename $0`
+
+# Backups Configuration
 BACKUPS_NAME=$(date +"%Y-%m-%d")
 NOW=$(date +"%Y/%m/%d %T")
+
+# Binary file path and option
 CMD=`which rsync`
 OPTION="-ahr -pog --delete --log-file="$PATH_LOG/$LOG_NAME.log
 
-# mkdir temporary files, log path , and destination backups
+# mkdir temporary files, log path , and destination backups if not already exist
 mkdir -p $TMP_BACKUPS
 mkdir -p $PATH_LOG
 mkdir -p $DEST_BACKUPS
